6209d0f902051a8030a990183787cec908775dfa
superkink3124 <ducnhimngn@gmail.com>
2023-03-02 21:17:52 +0700

    Add seed

--- src/main.py
+++ src/main.py
@@ -1,5 +1,4 @@
 import argparse
-
 from corpus.cdr_corpus import CDRCorpus
 from dataset.cdr_dataset import CDRDataset
 from config.cdr_config import CDRConfig
@@ -7,12 +6,24 @@ from dataset.utils import get_cdr_dataset, concat_dataset
 from dataset.collator import Collator
 from torch.utils.data import DataLoader
 from model.trainer import Trainer
+import torch
+import random
+import numpy as np
+
+
+def seed_all(seed):
+    np.random.seed(seed)
+    random.seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.manual_seed(seed)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--config", default="./data/config.json")
+    parser.add_argument("--seed", default=22, type=int)
     args = parser.parse_args()
+    seed_all(args.seed)
     config = CDRConfig.from_json_file(args.config)
     corpus = CDRCorpus(config)
     corpus.load_all_vocabs(config.saved_folder_path)
